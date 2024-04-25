ab8dbcb10338dfdf4b93d6951864476b7bbb462a
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-04-25 14:56:49 -0300

    MongoDB base setup

--- server/src/routes/homeRoutes.ts
+++ server/src/routes/homeRoutes.ts
@@ -1,17 +1,37 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Router, Request, Response } from "express";
+import { MongoClient } from "mongodb";
 
 const router = Router();
+const MONGODB_URL: string = process.env.MONGODB_URL as string;
 
-router.get("/", (req: Request, res: Response) => {
+const client = new MongoClient(MONGODB_URL);
 
-  axios
-    .get("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => {
-      // console.log(response.data);
-      res.json(response.data);
-    })
-    .catch((error) => console.log(error));
-});
+async function test() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db("local");
+  const collection = db.collection("testing");
+  let temp = await collection.findOne()
+  console.log(temp)
+}
+
+// router.get("/", (req: Request, res: Response) => {
+//   axios
+//     .get("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((response) => {
+//       // console.log(response.data);
+//       res.json(response.data);
+//     })
+//     .catch((error) => console.log(error));
+// });
+
+test()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 export default router;
