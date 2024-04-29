9c39bf518c0fa10e9babcddc1136b26144b4822a
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-04-29 17:39:37 -0300

    Updated userInterface

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -4,8 +4,13 @@ export interface UserInterface {
   _id?: ObjectId;
   email: string;
   password: string;
+  company: string;
+  status: "active" | "inactive";
+  registrationDate: Date;
+  formulasCreated: number;
+  lastAccess: Date;
 }
 
-export interface FormulaInterface{}
+export interface FormulaInterface {}
 
-export interface ColorInterface{}
+export interface ColorInterface {}
