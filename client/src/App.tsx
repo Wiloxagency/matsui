c27b1d44d1251ce84ba95575d29192dd8676b198
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-05-02 04:20:46 -0300

    Refactored routing

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Login } from "./Pages/Login/Login";
+import Formulas from "./Pages/Formulas/Formulas";
+import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
+import Header from "./Components/HeaderAndMainLayout/Header";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Header />}>
+        <Route index path="formulas" element={<Formulas />} />
+        <Route index path="admin" element={<AdminDashboard />} />
+      </Route>
+      <Route path="*" element={<Navigate to="login" />} />
+    </Routes>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,41 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Login } from "./Pages/Login/Login.tsx";
-import Header from "./Components/HeaderAndMainLayout/Header.tsx";
-import Formulas from "./Pages/Formulas/Formulas.tsx";
-import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard.tsx";
+import {
+  BrowserRouter,
+  // createBrowserRouter,
+  // RouterProvider,
+} from "react-router-dom";
+// import { Login } from "./Pages/Login/Login.tsx";
+// import Header from "./Components/HeaderAndMainLayout/Header.tsx";
+// import Formulas from "./Pages/Formulas/Formulas.tsx";
+// import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard.tsx";
 
 import { NextUIProvider } from "@nextui-org/system";
+import App from "./App.tsx";
 
-const router = createBrowserRouter([
-  { path: "*", element: <Login /> },
-  { path: "/", element: <Login /> },
-  {
-    path: "/",
-    element: <Header />,
-    children: [
-      {
-        path: "formulas",
-        element: <Formulas />,
-      },
-      {
-        path: "admin",
-        element: <AdminDashboard />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+// const router = createBrowserRouter([
+//   { path: "*", element: <Login /> },
+//   { path: "/", element: <Login /> },
+//   {
+//     path: "/",
+//     element: <Header />,
+//     children: [
+//       {
+//         path: "formulas",
+//         element: <Formulas />,
+//       },
+//       {
+//         path: "admin",
+//         element: <AdminDashboard />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/login",
+//     element: <Login />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <RouterProvider router={router}></RouterProvider>
+      {/* <RouterProvider router={router}></RouterProvider> */}
+      <BrowserRouter>
+        <App></App>
+      </BrowserRouter>
     </NextUIProvider>
   </React.StrictMode>
 );
