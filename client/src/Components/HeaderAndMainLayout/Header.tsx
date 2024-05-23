8e6e143722d7bb17b8d6a3ac28fe979c5a97f9e5
LeoLeto <132474553+LeoLeto@users.noreply.github.com>
2024-05-23 19:25:54 -0300

    Removed header from mobile

--- client/src/Components/HeaderAndMainLayout/Header.tsx
+++ client/src/Components/HeaderAndMainLayout/Header.tsx
@@ -2,8 +2,11 @@ import "./Header.scss";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import logo from "../../assets/matsui_logo.png";
 import spectrum from "../../assets/spectrum.png";
+import { useMediaQuery } from "react-responsive";
 
 export default function Header() {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
   const pages = [
     { path: "/formulas", label: "FORMULAS" },
     { path: "/admin", label: "ADMIN DASHBOARD" },
@@ -14,35 +17,39 @@ export default function Header() {
 
   return (
     <>
-      <div className="headerContainer">
-        <div className="logoAndNavbar">
-          <img src={logo} className="logo" />
-          <nav>
-            <ul>
-              {pages.map((page) => {
-                return (
-                  <li
-                    key={page.path}
-                    style={
-                      location.pathname === page.path
-                        ? { textDecoration: "underline" }
-                        : { textDecoration: "none" }
-                    }
-                  >
-                    <Link to={page.path}>{page.label}</Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
+      {isMobile ? (
+        <></>
+      ) : (
+        <div className="headerContainer">
+          <div className="logoAndNavbar">
+            <img src={logo} className="logo" />
+            <nav>
+              <ul>
+                {pages.map((page) => {
+                  return (
+                    <li
+                      key={page.path}
+                      style={
+                        location.pathname === page.path
+                          ? { textDecoration: "underline" }
+                          : { textDecoration: "none" }
+                      }
+                    >
+                      <Link to={page.path}>{page.label}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
 
-          <p className="email">Email@email.com</p>
-          <Link to="/login" className="logout">
-            Log out
-          </Link>
+            <p className="email">Email@email.com</p>
+            <Link to="/login" className="logout">
+              Log out
+            </Link>
+          </div>
+          <img src={spectrum} className="spectrumBar" />
         </div>
-        <img src={spectrum} className="spectrumBar" />
-      </div>
+      )}
       <div
         className="outletContainer"
         // style={
